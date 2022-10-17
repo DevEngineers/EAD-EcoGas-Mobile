24c7db1401144f1de13ebdc0d217efe20114a130
Mohamed Akeel <akeel1447@gmail.com>
2022-10-17 12:57:45 +0530

    Designing station owner home page

--- app/src/main/java/com/example/ecogas/StationOwnerHome.java
+++ app/src/main/java/com/example/ecogas/StationOwnerHome.java
@@ -2,13 +2,29 @@ package com.example.ecogas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StationOwnerHome extends AppCompatActivity {
+    Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_owner_home);
+
+        btnUpdate=findViewById(R.id.updateState);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**Redirecting to update fuel from via Intent**/
+                Intent intent = new Intent(StationOwnerHome.this,UpdateFuelStatus.class);
+                startActivity(intent);
+            }
+        });
     }
 }
