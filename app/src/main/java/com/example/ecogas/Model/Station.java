3e62d4b6b7ed5634cfbbc144996efd0fdba4f666
Zumry <amzumry@gmail.com>
2022-10-19 19:00:19 +0530

    init Station model class

--- app/src/main/java/com/example/ecogas/Model/Station.java
+++ app/src/main/java/com/example/ecogas/Model/Station.java
@@ -19,4 +19,92 @@ public class Station {
         this.stationName = stationName;
         this.stationLocation = stationLocation;
     }
+
+    public String getStationID() {
+        return stationID;
+    }
+
+    public void setStationID(String stationID) {
+        this.stationID = stationID;
+    }
+
+    public String getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(String ownerID) {
+        this.ownerID = ownerID;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getStationLocation() {
+        return stationLocation;
+    }
+
+    public void setStationLocation(String stationLocation) {
+        this.stationLocation = stationLocation;
+    }
+
+    public Fuel getPetrol() {
+        return petrol;
+    }
+
+    public void setPetrol(Fuel petrol) {
+        this.petrol = petrol;
+    }
+
+    public Fuel getSuperPetrol() {
+        return superPetrol;
+    }
+
+    public void setSuperPetrol(Fuel superPetrol) {
+        this.superPetrol = superPetrol;
+    }
+
+    public Fuel getDiesel() {
+        return diesel;
+    }
+
+    public void setDiesel(Fuel diesel) {
+        this.diesel = diesel;
+    }
+
+    public int getPetrolQueue() {
+        return petrolQueue;
+    }
+
+    public void setPetrolQueue(int petrolQueue) {
+        this.petrolQueue = petrolQueue;
+    }
+
+    public int getSuperPetrolQueue() {
+        return superPetrolQueue;
+    }
+
+    public void setSuperPetrolQueue(int superPetrolQueue) {
+        this.superPetrolQueue = superPetrolQueue;
+    }
+
+    public int getDieselQueue() {
+        return dieselQueue;
+    }
+
+    public void setDieselQueue(int dieselQueue) {
+        this.dieselQueue = dieselQueue;
+    }
 }
