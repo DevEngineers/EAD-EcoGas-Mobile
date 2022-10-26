72749b17ab1ea20e86e16933551e23873f96f0bb
Zumry <amzumry@gmail.com>
2022-10-26 18:07:39 +0530

    Done add queue methods and Increase queue count

--- app/src/main/java/com/example/ecogas/DetailStation.java
+++ app/src/main/java/com/example/ecogas/DetailStation.java
@@ -1,34 +1,37 @@
 package com.example.ecogas;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.ecogas.Model.Fuel;
 import com.example.ecogas.Model.Queues;
 import com.example.ecogas.Model.Station;
 import com.example.ecogas.Service.QueueService;
 import com.example.ecogas.Service.StationService;
-
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-import java.util.Objects;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * This is View Full Station details and add to queue screen java file to provide logic station_detail_layout layout xml
+ * This Screen is add the queue in a station by the user
+ * @Author: IT19175126 Zumry A.M
+ */
 
 public class DetailStation extends AppCompatActivity {
 
@@ -72,14 +75,14 @@ public class DetailStation extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 fuelName = adapterView.getSelectedItem().toString();
-                Log.d("TAG","=========------------------==============.........." + fuelName);
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.8.100:29193/Station/").addConverterFactory(GsonConverterFactory.create()).build();
+        /** Api call to retrieve the details of the station fuel details **/
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(SessionApplication.getApiUrl()+"Station/").addConverterFactory(GsonConverterFactory.create()).build();
         StationService stationService = retrofit.create(StationService.class);
         Call<Station> call = stationService.getStationDetails(stID);
         call.enqueue(new Callback<Station>() {
@@ -88,7 +91,8 @@ public class DetailStation extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     Station station = response.body();
 
-                    station.setId(stationID);
+                    /** Setting all the fetched data in to the textViews in the screen **/
+                    stationID = station.getId();
                     StName.setText(station.getStationName());
                     StLocation.setText(station.getLocation());
                     StPetrol92A.setText(station.getFuel().get(0).getCapacity());
@@ -99,54 +103,72 @@ public class DetailStation extends AppCompatActivity {
                     StPetrol95Q.setText(String.valueOf(station.getSuperPetrolQueue()));
                     StDieselQ.setText(String.valueOf(station.getDieselQueue()));
                     StSDieselQ.setText(String.valueOf(station.getSuperDieselQueue()));
-
                 }
             }
-
             @Override
             public void onFailure(Call<Station> call, Throwable t) {
 
             }
-
         });
 
-
         btnAddQueue.setOnClickListener(view -> {
+            /** Date Format */
+            Date todayDate = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+            String date = format.format(todayDate);
+
+            /** Time Format */
+            SimpleDateFormat formatTime = new SimpleDateFormat("h:mm a");
+            String nowTime = formatTime.format(Calendar.getInstance().getTime());
+
             Queues queues = new Queues();
-            queues.setId(stationID);
+            queues.setStationID(stationID);
             queues.setUserID(SessionApplication.getUserID());
             queues.setFuelName(fuelName);
-            queues.setArrivalDate(String.valueOf(Calendar.getInstance()));
-            queues.setArrivalTime(String.valueOf(Calendar.getInstance().getTime()));
+            queues.setArrivalDate(date);
+            queues.setArrivalTime(nowTime);
 
-            Log.d("TAG" , "----------------============Fuel name-------- " +queues.getFuelName() );
-            Log.d("TAG" , "----------------============ST ID-------- " +queues.getId());
-
-            if(fuelName==null){
+            if(fuelName.equals("Select Fuel Type")){
                 Toast.makeText(DetailStation.this,"Please select Fuel Type!!!", Toast.LENGTH_SHORT).show();
             }else{
-                Retrofit retrofitQ = new Retrofit.Builder().baseUrl("http://192.168.8.100:29193/Queue/").addConverterFactory(GsonConverterFactory.create()).build();
+                /** adding queue methods */
+                Retrofit retrofitQ = new Retrofit.Builder().baseUrl(SessionApplication.getApiUrl()).addConverterFactory(GsonConverterFactory.create()).build();
                 QueueService queueService = retrofitQ.create(QueueService.class);
                 Call<Queues> callQ = queueService.addUserToQueue(queues);
                 callQ.enqueue(new Callback<Queues>() {
                     @Override
                     public void onResponse(Call<Queues> callQ, Response<Queues> response) {
                         if (response.isSuccessful()) {
-                            Toast.makeText(DetailStation.this,"Joined Queue Successfully", Toast.LENGTH_SHORT).show();
+
+                            Fuel fuel = new Fuel();
+                            fuel.setFuelName(queues.getFuelName());
+
+                            /** call Increase queue count methods in Station */
+                            Retrofit retrofit1 = new Retrofit.Builder().baseUrl(SessionApplication.getApiUrl()+"Station/").addConverterFactory(GsonConverterFactory.create()).build();
+                            StationService stationService1 = retrofit1.create(StationService.class);
+                            Call<Station> call1 = stationService1.increaseFuelQueueCount(stationID, fuel);
+                            call1.enqueue(new Callback<Station>() {
+                                @Override
+                                public void onResponse(Call<Station> call, Response<Station> response) {
+                                    if (response.isSuccessful()){
+                                        Toast.makeText(DetailStation.this,"Joined Queue Successfully", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(DetailStation.this,ViewStations.class);
+                                        startActivity(intent);
+                                    }else{
+                                        Toast.makeText(DetailStation.this,"Unable to joint the queue ", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                @Override
+                                public void onFailure(Call<Station> call, Throwable t) {
+                                }
+                            });
                         }
                     }
-
                     @Override
                     public void onFailure(@NonNull Call<Queues> callQ, Throwable t) {
-
                     }
                 });
             }
         });
-
-
     }
-
-
-
 }
